068f96aa8a1de307330ed5b379360afc03d7b756
Oshank Kumar <oshankfriends@gmail.com>
2026-05-02 22:06:40 +0530

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.go
@@ -0,0 +1,20 @@
+
+func numRescueBoats(people []int, limit int) int {
+	sort.Ints(people)
+
+	var count int
+	left, right := 0, len(people)-1
+
+	for left <= right {
+		sum := people[left] + people[right]
+		if sum <= limit {
+			left++
+			right--
+		} else {
+			right--
+		}
+		count++
+	}
+	
+	return count
+}
