9b20b756333cfde04ced384603864c6a515f7ffb
Oshank Kumar <oshankfriends@gmail.com>
2026-08-06 23:13:28 +0530

    Add: unique-paths-ii - submission-2

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-2.go
@@ -0,0 +1,39 @@
+
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	memo := make([][]int, len(obstacleGrid))
+	for i := range memo {
+		memo[i] = make([]int, len(obstacleGrid[0]))
+	}
+	return findUniquePathsWithObstacles(0, 0, obstacleGrid, memo)
+}
+
+func findUniquePathsWithObstacles(i, j int, obstacleGrid [][]int, memo [][]int) int {
+	if i >= len(obstacleGrid) || j >= len(obstacleGrid[0]) {
+		return 0
+	}
+
+	if obstacleGrid[i][j] == 1 {
+		return 0
+	}
+
+	if i == len(obstacleGrid)-1 && j == len(obstacleGrid[0])-1 {
+		return 1
+	}
+
+	if memo[i][j] != 0 {
+		return memo[i][j]
+	}
+
+	var count int
+	if i+1 < len(obstacleGrid) && obstacleGrid[i+1][j] == 0 {
+		count += findUniquePathsWithObstacles(i+1, j, obstacleGrid, memo)
+	}
+
+	if j+1 < len(obstacleGrid[0]) && obstacleGrid[i][j+1] == 0 {
+		count += findUniquePathsWithObstacles(i, j+1, obstacleGrid, memo)
+	}
+
+	memo[i][j] = count
+
+	return count
+}
